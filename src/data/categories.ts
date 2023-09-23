c0a46279800bf60623b9695fc6df19689c9d9ef2
Arkryonia <arkryonia@gmail.com>
2023-09-23 13:23:19 +0100

    :fire: Remove demo files

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,11 @@
 // List of categories for blog posts
-export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
-] as const
+
+export const CATEGORIES = ['Ọgbọn', 'Iṣa', 'Oduduwa', 'Ọnàáwó', 'Babáláwó'] as const
+
+// export const CATEGORIES = [
+// 	'Category 1',
+// 	'Category 2',
+// 	'Category 3',
+// 	'Category 4',
+// 	'Category 5',
+// ] as const
